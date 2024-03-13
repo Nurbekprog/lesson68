8a2b4e9ac9dfb339805a6dc1b6a8d280500f77cc
Nurbekprog <nurprogdata@gmail.com>
2024-03-13 08:49:04 +0500

    dss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center'>TODOS</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='fs-bolder text-center'>Hello</h1>
+    </div>
+  )
+}
+
+export default Header
